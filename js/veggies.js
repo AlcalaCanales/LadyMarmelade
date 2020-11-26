49fa5d18358b120af26fefef706e5940b3108c32
Adriana Alcala <a.alcalacanales@gmail.com>
2020-11-26 21:43:27 +0100

    implementing veggies

--- js/veggies.js
+++ js/veggies.js
@@ -5,12 +5,26 @@ class Veggie {
       this.row=row;
     }
     draw () {
+      const veggieImage = new Image();
+      veggieImage.src = 'images/Strawberry.png';
+      veggieImage.addEventListener('load', () => {
+
+          this.game.context.drawImage(
+            veggieImage,
+              (this.col * 50)+5,
+              (this.row *50)+5,
+              40,
+              40);
+      });
+
+
+      /*
       this.game.context.fillStyle = 'coral';
       this.game.context.fillRect(
         this.col*50+5,
         this.row*50+5,
         40, 
         40
-      );
+      );*/
     }
 }
